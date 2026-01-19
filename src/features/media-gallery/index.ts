88120241094bcdc5e9b12a09edbf7fac38e3b71b
DMYTRO_VIVCHARYK <dmitrovivcarik@gmail.com>
2026-01-19 23:28:07 +0200

    Fix: Cleaned no needed import

--- src/features/media-gallery/index.ts
+++ src/features/media-gallery/index.ts
@@ -1,5 +1,4 @@
 // Public API of media-gallery feature
 export { MediaGallery } from './view/MediaGallery'
-export { MediaGalleryGate } from './view/gates/MediaGalleryGate'
 export { MediaGalleryProvider } from './model/MediaGalleryProvider'
 export { useMediaGalleryViewModel } from './model/useMediaGalleryViewModel'
